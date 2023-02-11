a16af2083e12f7f529c1a7a426a263a7dc55196d
cmanikon <cmanikon@CMANIKON-T490.us.oracle.com>
2023-02-11 15:36:08 +0530

    first git commit to push the project code

---
+++ src/Package1/Project.java
@@ -0,0 +1,105 @@
+package Package1;
+
+
+import java.util.*;
+
+public class Project {
+	
+	public static void main(String[] args) {
+//		LinkedList<String> list = new LinkedList<>();
+//		list.add("Red");
+//		list.add("White");
+//		list.add("Yellow");
+//		list.add("Blue");
+//		System.out.println(list);
+		
+//		HashMap<Integer, String> map = new HashMap<>();
+//		map.put(1, "Virat");
+//		map.put(3, "Dhoni");	
+//		map.put(4, "Sehwag");
+//		//System.out.println(map.get(1).charAt(2));
+//		System.out.println(map.size())
+//		HashMap<Integer, String> map2 = new HashMap<>();
+//		map2.putAll(map);
+//		System.out.println(map2.isEmpty());
+//		System.out.println(map2.values());
+		int[] arr = {1,2,3};
+		System.out.println(arr.length);
+		double leng = arr.length;
+		System.out.println(leng);
+		double sum = 8/leng;
+		System.out.println(sum);
+//		class sortkey implements Comparator<String>{
+//			public int compare(String str1, String str2) {
+//				return str1.compareTo(str2);
+//			}
+//		}
+//		
+		//System.out.println(map.containsKey(5));
+//		for(Map.Entry<Integer, String> adsf :map.entrySet()) {
+//			System.out.println(adsf.getKey() + " " + adsf.getValue());
+//		}
+		
+//		
+//		PriorityQueue<Integer> pqueue = new PriorityQueue<>(10, Collections.reverseOrder());
+//		pqueue.add(1);
+//		pqueue.add(2);
+//		pqueue.add(3);
+//		pqueue.add(4);
+//		pqueue.add(5);
+//		pqueue.add(6);
+//		pqueue.add(1);
+//		pqueue.add(2);
+//		pqueue.add(3);
+//		pqueue.add(4);
+//		pqueue.add(5);
+//		pqueue.add(6);
+//		System.out.println(pqueue);
+//		System.out.println(pqueue.size());
+//		while(pqueue!=null && pqueue.size()>0) {
+//			System.out.println(pqueue.poll());
+//		}
+		//String str = pqueue.toString();
+//		System.out.println(str);
+//		System.out.println(str.length());
+//		System.out.println(str.getClass().getSimpleName());
+//		ArrayList<String> arr = new ArrayList<>(pqueue);
+//		System.out.println(arr);
+//		System.out.println(arr.getClass().getSimpleName());
+////		TreeSet<Integer> numbers = new TreeSet<>();
+//		numbers.add(1);
+//		numbers.add(2);
+//		numbers.add(3);
+//		numbers.add(5);
+//		numbers.add(6);
+//		numbers.add(7);
+//		numbers.add(8);
+//		numbers.add(9);
+//		numbers.add(11);
+//		System.out.println(numbers);
+//		System.out.println(numbers.remove(2));
+//		System.out.println(numbers);
+//		
+		
+		
+//		System.out.println(colors.first());
+//		System.out.println(colors.last());
+//		System.out.println(colors.pollFirst());
+//		System.out.println(colors.ceiling("Blue"));
+//		Iterator<String> itr = colors.descendingIterator();
+//		while(itr.hasNext()) {
+//			System.out.println(itr.next());
+//			
+//		}
+//		
+		
+//		Iterator<String> itr = colors.iterator();
+//		while(itr.hasNext()) {
+//			itr.next();
+//			itr.remove();
+//		}
+//		System.out.println(colors);
+	}
+	
+
+}
---
+++ src/Package1/Relearn.java
@@ -0,0 +1,16 @@
+package Package1;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Relearn {
+	public static void main(String[] args) {
+//		//HashSet<int> a1 = {1, 4, 17, 7, 25, 3, 100};
+//		HashSet a = new HashSet();
+//		a = a1;
+//		System.out.println(a);
+//		
+	}
+}
+
+
